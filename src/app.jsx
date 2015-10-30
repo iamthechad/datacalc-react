5fc8b17eb78894a268f4c82a637723aeb01b0b69
Chad Johnston <cjohnston@megatome.com>
2015-10-30 14:31:11 -0600

    Starting to add behavior.
    It's broken now.

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,34 @@
 var React = require('react');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var rootUrl = 'https://glaring-torch-2436.firebaseio.com/';
 
 var App = React.createClass({
-    render: function () {
-        return <h1>
-            Hello, React!
-        </h1>
+    mixins: [ ReactFire ],
+    getInitialState: function() {
+        return {
+            items: {},
+            loaded: false
+        }
+    },
+    componentWillMount: function() {
+        this.fb = new Firebase(rootUrl + 'categories/');
+        //this.bindAsArray(this.fb, 'categories');
+        this.fb.on('value', this.handleDataLoaded);
+    },
+    render: function() {
+        return <div className="row panel panel-default">
+            <div className="col-md-8 col-md-offset-2">
+                <h2 className="text-center">
+                    Categories
+                </h2>
+            </div>
+        </div>
+    },
+    handleDataLoaded: function(snapshot){
+        console.log(snapshot.val());
+        //console.log(this.firebaseRefs.categories);
+        this.setState({loaded: true});
     }
 });
 
