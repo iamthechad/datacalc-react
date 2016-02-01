5728243928cff7278ed07745cdaef6b847b3eedf
Chad Johnston <cjohnston@megatome.com>
2016-02-01 11:28:52 -0700

    Add icons for categories

--- scripts/components/Catalog.js
+++ scripts/components/Catalog.js
@@ -1,18 +1,34 @@
 import React from 'react';
 
+import Card from 'material-ui/lib/card/card';
+import CardTitle from 'material-ui/lib/card/card-title';
+import CardText from 'material-ui/lib/card/card-text';
 import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
 import {SelectableContainerEnhance} from 'material-ui/lib/hoc/selectable-enhance';
+import FontIcon from 'material-ui/lib/font-icon';
 
 let SelectableList = SelectableContainerEnhance(List);
 
 import Category from './Category';
 
 const Catalog = props => (
-  <SelectableList valueLink={{value: props.selectedCategory, requestChange: props.onCategorySelect}}>
-    {Object.keys(props.catalog.categories).sort().map(key =>
-      <ListItem key={key} value={key} primaryText={props.catalog.categories[key].name}/>)}
-  </SelectableList>
+  <Card>
+    <CardTitle title="Categories" />
+    <CardText>
+      <SelectableList valueLink={{value: props.selectedCategory, requestChange: props.onCategorySelect}}>
+        {Object.keys(props.catalog.categories).sort().map(key =>
+          <ListItem
+            key={key}
+            value={key}
+            primaryText={props.catalog.categories[key].name}
+            leftIcon={<FontIcon
+              className={props.catalog.categories[key].icon}
+            />}
+          />)}
+      </SelectableList>
+    </CardText>
+  </Card>
 );
 
 Catalog.propTypes = {
